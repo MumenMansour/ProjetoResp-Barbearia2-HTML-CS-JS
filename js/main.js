393e71c26f9ceb512142c0ce5f2deef373fbb2b0
MumenMansour <mumenmansour97@gmail.com>
2022-11-16 19:36:52 -0300

    Changes to be committed: 	new file:   .vscode/settings.json 	new file:   css.txt 	new file:   css/reset.css 	new file:   css/style.css 	new file:   html.txt 	new file:   img/Thumbs.db 	new file:   img/about.webp 	new file:   img/fotos/23.jpg 	new file:   img/fotos/44.jpg 	new file:   img/fotos/49.jpg 	new file:   img/fotos/Thumbs.db 	new file:   img/topo.jpg 	new file:   index.html 	new file:   js/main.js

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//document.body.style.background = "red"
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for ( const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+}
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+    })
+}
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+//console.log(navHeight)
+
+function sombraHeader() {
+    if (window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
+}
+
+window.addEventListener('scroll', function(){
+    sombraHeader()
+})
